62b68ae5e86c1cea58e3ad1eb253d7ca208bf9ea
jefferson echavarria <jfrson__@live.com>
2023-05-23 07:21:00 -0500

    CRUD Book created.

---
+++ Library/Library/Controllers/BooksController.cs
@@ -0,0 +1,155 @@
+﻿using Library.DAL;
+using Library.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Controllers
+{
+    public class BooksController : Controller
+    {
+        #region Constants
+        private readonly DataBaseContext _context;
+        #endregion
+
+        #region Builder
+        public BooksController(DataBaseContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        #region Book actions
+        // GET: Books
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return _context.Books != null ?
+                        View(await _context.Books.ToListAsync()) :
+                        Problem("Entity set 'DataBaseContext.Books'  is null.");
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Books/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    book.CreatedDate = DateTime.Now;
+                    _context.Add(book);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                        ModelState.AddModelError(string.Empty, "Ya existe un libro con el mismo nombre.");
+                    else
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+            return View(book);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null || _context.Books == null) return NotFound();
+
+            var book = await _context.Books.FindAsync(id);
+
+            if (book == null) return NotFound();
+
+            return View(book);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Book book)
+        {
+            if (id != book.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(book);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                        ModelState.AddModelError(string.Empty, "Ya existe un libro con el mismo nombre.");
+                    else
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
+            }
+            return View(book);
+        }
+
+        // GET: Books/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Books == null) return NotFound();
+
+            var book = await _context.Books.FirstOrDefaultAsync(b => b.Id.Equals(id));
+
+            if (book == null) return NotFound();
+
+            return View(book);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Books == null) return NotFound();
+
+            var book = await _context.Books.FirstOrDefaultAsync(b => b.Id.Equals(id));
+
+            if (book == null) return NotFound();
+
+            return View(book);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Books == null) return Problem("Entity set 'DataBaseContext.Books'  is null.");
+
+            var book = await _context.Books.FindAsync(id);
+
+            if (book != null) _context.Books.Remove(book);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(Guid id)
+        {
+            return (_context.Books?.Any(b => b.Id.Equals(id))).GetValueOrDefault();
+        }
+        #endregion
+    }
+}
