a3eaf507d3fd0e33bccc0a997c74d76030b3d2a2
jefferson echavarria <jfrson__@live.com>
2023-06-09 07:53:26 -0500

    Booking actions created

--- Library/Library/Controllers/BookingsController.cs
+++ Library/Library/Controllers/BookingsController.cs
@@ -31,6 +31,17 @@ namespace Library.Controllers
                         Problem("Entity set 'DataBaseContext.LoanDetails'  is null.");
         }
 
+        public async Task<IActionResult> Loan()
+        {
+            return _context.LoanDetails != null ?
+                        View(await _context.LoanDetails
+                        .Include(ld => ld.Loan)
+                        .ThenInclude(l => l.User)
+                        .Include(ld => ld.Book)
+                        .ToListAsync()) :
+                        Problem("Entity set 'DataBaseContext.Loan'  is null.");
+        }
+
         public async Task<IActionResult> Delete(Guid? id)
         {
             LoanDetail loanDetail = await _context.LoanDetails
@@ -43,7 +54,42 @@ namespace Library.Controllers
 
             _context.LoanDetails.Remove(loanDetail);
             await _context.SaveChangesAsync();
+            return View();
+            //return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Confirm(Guid? id)
+        {
+            LoanDetail loanDetail = await _context.LoanDetails
+                .Include(ld => ld.Book)
+                .Include(ld => ld.Loan)
+                .FirstOrDefaultAsync(ld => ld.Id.Equals(id));
+
+            if (loanDetail == null || id == null) return NotFound();
+
+            loanDetail.Loan.LoanStatus = Enum.LoanStatus.Confirmado;
+            loanDetail.CreatedDate = DateTime.Now;
+            loanDetail.Deadline = DateTime.Now.AddDays(5);
+
+            _context.LoanDetails.Update(loanDetail);
+            await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Return(Guid? id)
+        {
+            LoanDetail loanDetail = await _context.LoanDetails
+                .Include(ld => ld.Book)
+                .FirstOrDefaultAsync(ld => ld.Id.Equals(id));
+
+            if (loanDetail == null || id == null) return NotFound();
+
+            loanDetail.Book.Stock += loanDetail.Quantity;
+
+            _context.LoanDetails.Remove(loanDetail);
+            await _context.SaveChangesAsync();
+            
+            return View();
         }
     }
 }
